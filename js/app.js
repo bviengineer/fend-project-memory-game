08dbd57b4a90739f73be8944b671bbad97bf5fb6
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-06-28 13:56:14 -0400

    Reward player up to 4 additional points for 0 to no more than 2 incorrect guesses

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const deckLIs = document.getElementsByClassName("card"), //deck of cards listed 
 //Trackers & variables
 let playerMovesCount = 0, //tracks the # of clicks player makes regardless of a match
     playerPoints = 0, //track points for # of correct guesses
-    playerPenalty = 0, //deducts points for incorrect guesses
+    playerPenaltyPoints = 0, //deducts points for incorrect guesses
     incorrectGuesses = 0, //keeps tracks of the number of incorrect guesses
     buttonClicked; //used in showCards func to hold the click event for each card 
     
@@ -68,9 +68,7 @@ function showCard(){
     for(let i = 0; i < indexedCards.length; i++){        
         indexedCards[i].addEventListener("click", function(){
             buttonClicked = indexedCards[i]; 
-            
-            console.log(buttonClicked.classList.value);
-            
+                        
             //checks for duplicate clicks
             if(buttonClicked.className === "card open show" || buttonClicked.className === "card open show match"){
                 invalidMove();
@@ -83,10 +81,11 @@ function showCard(){
             if(cardsOpened.length < 2){
                 cardsOpened[0].classList += " open show";                            
             } else if (cardsOpened.length === 2 && cardsOpened[0].childNodes[1].className === cardsOpened[1].childNodes[1].className){
-                openCards();                  
+                openCards();
+                myCardMatches();                  
                 setTimeout(aMatch, 500);
-                myCardMatches();                                
-                pointsEarned();                
+                pointsEarned();
+                starRating();
                 setTimeout(gameOver, 800);
             } else if(cardsOpened.length === 2 && cardsOpened[0].childNodes[1].className !== cardsOpened[1].childNodes[1].className){
                 openCards()
@@ -98,38 +97,43 @@ function showCard(){
         });
     }
 }
-/* stand-alone EVENT LISTENERS in alaphabetical order */
+/* [stand-alone] EVENT LISTENERS, in alaphabetical order */
 
-//close modal/game status window
+//closes modal/game status window
 closeModal.addEventListener("click", function(){
     modal.style.display = "none";    
 });
 
-//restart game from modal
+//resets game from modal
 playAgainButton.addEventListener("click", function(){
     modal.style.display = "none";
+    
     //restart game timer
-    resetStarRating();
-    resetMovesCount();  
     resetGameBoard();
-    resetMyCardMatches();
     resetPointsEarned();
+    resetWrongGuesses();
+    resetPenaltyPoints();
     resetStarRating();
+    resetMovesCount();
+    resetMyCardMatches(); 
 });
 
-//resets game board, timer & star rating
+//resets game from button on page
 restartGameButton.addEventListener("click", function(){
+    
     //restarts game timer
-    resetStarRating();
-    resetMovesCount();    
     resetGameBoard();
-    resetMyCardMatches();
     resetPointsEarned();
+    resetWrongGuesses();
+    resetPenaltyPoints();
     resetStarRating();
+    resetMovesCount();
+    resetMyCardMatches();   
 });
  
  
-/* FUNCTIONS in alphabetical order*/
+/* FUNCTIONS BEGIN HERE:
+They are in alphabetical order with their corresponding reset counterpart listed immediately after, if any */
 
 //ensures matched cards remain open
 function aMatch(){
@@ -140,7 +144,7 @@ function aMatch(){
     clearArray();
 }
 
-//clears CardsOpened array after which holds cards to be compared for a match
+//clears CardsOpened array after two cards are compared for a match
 function clearArray() {
     cardsOpened.splice(0);
 }
@@ -157,7 +161,7 @@ function colorChange(){
 function displayModal(){
     modal.style.display = "inline";
    
-    modalContent.innerHTML ="<p>Great job! <br> Points earned:  <strong>" + playerPoints + "</strong></p> <p>You made:  <strong>" + playerMovesCount + "</strong> moves</p><p>The number of incorrect guesses were: <strong>"+ incorrectGuesses + "</strong></p";
+    modalContent.innerHTML ="<p>Great job! <br> Points earned:  <strong>" + playerPoints + "</strong></p> <p>You made:  <strong>" + playerMovesCount + "</strong> moves</p><p>Numeber of incorrect guesses: <strong>"+ incorrectGuesses + "</strong></p";
     
     //creates, styles and append to modal, button to restart game from modal  
     playAgainButton.innerHTML = "<strong>Play Again</strong>";
@@ -166,7 +170,7 @@ function displayModal(){
     playAgainButton.style.color = "white";
     modalContent.appendChild(playAgainButton);   
 
-    //hides close window text on modal since modal will be closed by Play Again button
+    //hides "Close Window" text on modal since modal will be closed by "Play Again" button
     closeModal.style.display = "none";
 
     /*
@@ -178,21 +182,38 @@ function displayModal(){
 
 //dislays modal once all cards are successfully matched
 function gameOver(){
-    if(myMatches.length === 16){
-       displayModal();    
-    }   
+    if(myMatches.length === 16 && incorrectGuesses === 0){
+        //player earns an addt'l 4-pts if the # no. of incorrect guesses are <= 2
+        playerPoints += 4; 
+        displayModal();
+    } 
+        else if(myMatches.length === 16 && incorrectGuesses === 1){
+            playerPoints += 2;
+            displayModal();    
+        }
+        else if(myMatches.length === 16 && incorrectGuesses === 2){
+            playerPoints += 1;
+            displayModal();    
+        }
+        else if(myMatches.length === 16 && incorrectGuesses > 2){
+        displayModal();    
+        }
 }
 
 //incorrect guesses tracker
 function incorrectNoGuesses(){
     incorrectGuesses += 1;
 }
+//resets incorrect guesses
+function resetWrongGuesses(){
+    incorrectGuesses = 0;
+}
 
 //duplicate card selections notification
 function invalidMove(){
-    modalContent.innerHTML ="<p>Please try again! That card has already been selected. <br> Points earned:  <strong>" + playerPoints + "</strong></p> <p>You made:  <strong>" + playerMovesCount + "</strong> moves</p>";
-
     modal.style.display = "inline";
+
+    modalContent.innerHTML ="<p>Please try again! That card has already been selected. <br> Points earned:  <strong>" + playerPoints + "</strong></p> <p>You've made:  <strong>" + playerMovesCount + "</strong> moves</p>";
 }
 
 //tracks & displays number of moves player makes 
@@ -200,14 +221,23 @@ function movesCount(){
     playerMovesCount += 1;
     playerMoves.innerHTML = playerMovesCount;
 }
+//resets player moves
+function resetMovesCount(){
+    playerMovesCount = 0;
+    playerMoves.innerHTML = playerMovesCount;
+}
 
-//arry of all card matches/pairs
+//array of matched cards/pairs
 function myCardMatches(){
     myMatches.push(cardsOpened[0]);
     myMatches.push(cardsOpened[1]);
 }
+//resets array of matched cards
+function resetMyCardMatches(){
+    myMatches.splice(0);
+}
 
-//closes unmatched cards
+//closes unmatched cards & clears the cardsOpened array
 function notAMatch(){
     cardsOpened[0].classList = "";
     cardsOpened[1].classList = "";
@@ -216,7 +246,7 @@ function notAMatch(){
     clearArray();
 }
 
-//opens cards selected
+//opens selected cards
 function openCards(){
     cardsOpened[0].classList = "";
     cardsOpened[1].classList = "";
@@ -224,27 +254,22 @@ function openCards(){
     cardsOpened[1].classList += "card open show";
 }
 
-//deducts points for incorrect guesses
+//deducts points for incorrect guesses & for use with starRating function
 function playerPenaltyFunc(){
-    playerPoints -= 2;
+   playerPenaltyPoints -= 2;
+}
+//resets penalty points 
+function resetPenaltyPoints(){
+    playerPenaltyPoints = 0;
 }
 
-//player points earned 
-function pointsEarned(){
-    playerPoints += 8;
-    if(playerPoints > 12 && playerPoints <= 16){
-        scoreStars[0].style.color = "yellow";
-        scoreStars[1].style.display = "none";
-        scoreStars[2].style.display = "none";
-    } else if(playerPoints > 8 && playerPoints <= 12){
-        scoreStars[0].style.display = "yellow";
-        scoreStars[1].style.color = "yellow";
-        scoreStars[2].style.display = "none";
-    } else if(playerPoints === 0){
-        scoreStars[0].style.color = "yellow";
-        scoreStars[1].style.color = "yellow";
-        scoreStars[2].style.color = "yellow";
-    }
+//tracks player points for correct guesses
+function pointsEarned   (){
+    playerPoints += 12;
+}
+//resets playerPoints 
+function resetPointsEarned(){
+    playerPoints = 0;
 }
 
 //resets game board
@@ -255,23 +280,23 @@ function resetGameBoard(){
     }
 }
 
-//resets player moves
-function resetMovesCount(){
-    playerMovesCount = 0;
-    playerMoves.innerHTML = playerMovesCount;
+//player points earned was PointsEarned
+function starRating(){
+    if(playerPoints > 8 && playerPoints <= 24){
+        scoreStars[0].style.color = "yellow";
+        scoreStars[1].style.display = "none";
+        scoreStars[2].style.display = "none";
+    } else if(playerPoints > 24 && playerPoints <= 40){
+        scoreStars[0].style.display = "yellow";
+        scoreStars[1].style.color = "yellow";
+        scoreStars[2].style.display = "none";
+    } else if(playerPoints === 0){
+        scoreStars[0].style.color = "yellow";
+        scoreStars[1].style.color = "yellow";
+        scoreStars[2].style.color = "yellow";
+    }
 }
-
-//resets array with matched cards when player restarts game or selects play again
-function resetMyCardMatches(){
-    myMatches.splice(0);
-}
-
-//resets playerPoints
-function resetPointsEarned(){
-    playerPoints = 0;
-}
-
-//restart star rating
+//restart star rating 
 function resetStarRating(){
     for(let i = 0; i < scoreStars.length; i++){
         scoreStars[i].style.display = "inline";
@@ -286,8 +311,6 @@ function startGame(){
         console.log(gameTimerSecs.value);
     }
 }
-
-
 //My logic endds here
 
 /* UDACITY'S NOTES
