7a8f06f26f5209020e707c4810e09651c83fdd71
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-07-06 09:07:57 -0400

    Place randomly shuffled cards on page for each new game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //Collections & Arrays
 const deckLIs = document.getElementsByClassName("card"); //deck of cards listed as <li>s (HTMLCollection)
-    indexedCards = [], // used inside 1st for loop to capture the index of each <li> in the deck of LIs (deckLIs)
-    cardsOpened = [], //to hold and compare the opened cards
-    myMatches = []; //to hold matched cards
+    indexedCards = [], //to capture the index of each <li> in the deck of LIs (deckLIs)
+    cardsOpened = [], //to hold and compare opened cards
+    myMatches = []; //holds matched cards
 let shuffledDeck = []; //holds deck of shuffled cards
 
 //Trackers & variables
@@ -11,7 +11,7 @@ let playerMovesCount = 0, //tracks the # of clicks player makes regardless of a 
     totalPoints = 0; //holds calculation for playerPoints - playerPenaltyPoints
     playerPenaltyPoints = 0, //deducts points for incorrect guesses
     incorrectGuesses = 0, //keeps tracks of the number of incorrect guesses
-    finalStarRating = 0, //star rating to be presented after game buttonClicked; //used in showCards func to hold the click event for each card 
+    finalStarRating = 0, 
     minutes = 0,
     seconds = 0,
     hours = 0,
@@ -38,7 +38,7 @@ UDACITY'S NOTES
     - add each card's HTML to the page
  
 Shuffle function from http://stackoverflow.com/a/2450976 
-    Used like
+    Used like:
     var arr = [2, 11, 37, 42];
     arr = shuffle(arr);
     console.log(arr);
@@ -49,9 +49,9 @@ function shuffle(array) {
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = array[currentIndex].children[0].className;
+        array[currentIndex].children[0].className = array[randomIndex].children[0].className;
+        array[randomIndex].children[0].className = temporaryValue;
     }
     return array;
 }
@@ -65,7 +65,7 @@ showCard();
 //Loops through deck of HTMLCollections and places each item in an array
 function loopDeck(){
     for(let i = 0; i < deckLIs.length; i++) {
-        indexedCards.push(deckLIs[i]); //turns collection into an array
+        indexedCards.push(deckLIs[i]); //turns collection into an array element
     }
     return indexedCards;
 }
@@ -74,22 +74,18 @@ function loopDeck(){
 function listShuffledDeck(){
     shuffledDeck = indexedCards;
     shuffledDeck = shuffle(shuffledDeck);
-    console.log("deck b4 appending to page: ", shuffledDeck); //testiting
-    
-    for(let i = 0; i < shuffledDeck.length; i++){
-        deckUL.appendChild(shuffledDeck[i]);
-    }
-    console.log("deck after appending shuffled cards", deckUL); //testing    
+
+    deckUL.children = shuffledDeck;    
 }
 
-//show & hide cards
+//Showing & hiding of cards
 function showCard(event){
     for(let i = 0; i < shuffledDeck.length; i++){        
         shuffledDeck[i].addEventListener("click", function(){
             
             buttonClicked = shuffledDeck[i];
 
-            //checks for duplicate clicks
+            //Determines if card clicked is already opened or matched
             if(buttonClicked.className === "card open show" || buttonClicked.className === "card open show match"){
                 invalidMove();
             } else {
@@ -97,7 +93,7 @@ function showCard(event){
                 movesCount();
             }
             
-            //opening and closing of cards 
+            //Opening & closing of cards 
             if(cardsOpened.length < 2){
                 cardsOpened[0].classList += " open show";                            
             } else if (cardsOpened.length === 2 && cardsOpened[0].childNodes[1].className === cardsOpened[1].childNodes[1].className){
@@ -111,7 +107,7 @@ function showCard(event){
                 openCards();
                 colorChange();
                 incorrectNoGuesses();
-                playerPenaltyFunc();         
+                // playerPenaltyFunc();         
                 setTimeout(notAMatch, 700);                  
                 starRating();
                 playerPenaltyFunc();       
@@ -121,15 +117,16 @@ function showCard(event){
 }
 /* [stand-alone] EVENT LISTENERS, in alaphabetical order */
 
-//closes modal/game status window
+//Closes modal/game status window
 closeModal.addEventListener("click", function(){
     modal.style.display = "none";    
 });
 
-//resets game from modal
+//Resets game from modal
 playAgainButton.addEventListener("click", function(){
     modal.style.display = "none";
     resetGameBoard();
+    listShuffledDeck();
     resetPointsEarned();
     resetWrongGuesses();
     resetPenaltyPoints();
@@ -141,9 +138,10 @@ playAgainButton.addEventListener("click", function(){
     startTimer();
 });
 
-//resets game using restart button on page
+//Resets game using restart button on page
 restartGameButton.addEventListener("click", function(){
     resetGameBoard();
+    listShuffledDeck();
     resetPointsEarned();
     resetWrongGuesses();
     resetPenaltyPoints();
@@ -155,25 +153,26 @@ restartGameButton.addEventListener("click", function(){
     startTimer();
 });
 
- //start game timer from within modal
+ //Starts game from modal
  startGameButton.addEventListener("click", function(){
     startTimer();
     modal.style.display = "none";
 });
 
-//starts timer using start button located on timer
+//Starts timer using button on timer
 startTimerButton.addEventListener("click", function(){
     startTimer();
 });
 
+//Stops timer using button on timer
 stopTimerButton.addEventListener("click", function(){
     clearTimeout(time);
 });
  
-/* FUNCTIONS BEGIN HERE:
+/* FUNCTIONS BEGIN HERE
 Listed in alphabetical order with their corresponding reset counterpart listed immediately after, if any */
 
-//ensures matched cards remain open
+//Ensures matched cards remain open
 function aMatch(){
     cardsOpened[0].classList = "";
     cardsOpened[1].classList = "";
@@ -182,12 +181,12 @@ function aMatch(){
     clearArray();
 }
 
-//clears CardsOpened array after two cards are compared for a match
+//Clears CardsOpened array after two cards are compared for a match
 function clearArray() {
     cardsOpened.splice(0);
 }
 
-//changes color of cards when guesses are incorrect
+//Changes color of cards when guesses are incorrect
 function colorChange(){
     cardsOpened[0].classList = "";
     cardsOpened[1].classList = "";
@@ -195,7 +194,7 @@ function colorChange(){
     cardsOpened[1].classList += "card open show wrongMatch";
 }
 
-//displays game results
+//Displays game results
 function displayModal(){  
     clearTimeout(time); 
     modalContent.innerHTML ="<p>Great job! You matched all the cards.</p> <p>Points earned:  <strong>" + totalPoints + "</strong><br> Star rating: <strong> " + finalStarRating + "</strong><br> Moves made: <strong> " + playerMovesCount + "</strong> <br> Incorrect guesses: <strong>"+ incorrectGuesses + "</strong> <br> Time: <strong>" + gameTimerDisplay.innerText + "</strong></p";
@@ -210,7 +209,7 @@ function displayModal(){
     modal.style.display = "inline";
 }
 
-//appears after the page loads
+//Appears on initial page load
 function gameInstructions(){
     modalContent.innerHTML =`<p>Welcome to <strong>Memory Game</strong>. To play:
         <ul>
@@ -235,10 +234,14 @@ function gameInstructions(){
     modal.style.display = "inline";
 }
 
-//dislays modal once all cards are successfully matched
+/*Dislays modal with results once all cards are successfully matched
+-player earns an addt'l 4-pts if the # no. of incorrect guesses = 0 
+-OR, an addt'l 2-pts if the no. of incorrect guesses = 1
+-OR, an addt'l 1-pt if the no. of incorrecdt guesses = 2
+*/
 function gameOver(){
     if(myMatches.length === 16 && incorrectGuesses === 0){
-        //player earns an addt'l 4-pts if the # no. of incorrect guesses are <= 2
+        
         totalPoints += 4; 
         displayModal()
     } 
@@ -255,48 +258,120 @@ function gameOver(){
         }
 }
 
-//incorrect guesses tracker
+//Incorrect guesses tracker
 function incorrectNoGuesses(){
     incorrectGuesses += 1;
 }
-//resets incorrect guesses
+//Resets incorrect guesses
 function resetWrongGuesses(){
     incorrectGuesses = 0;
 }
 
-//duplicate card selections notification
+//Duplicate card selections notification
 function invalidMove(){
     modalContent.innerHTML ="<p>Please try again! That card has already been selected. <br> <p>You've made:  <strong>" + playerMovesCount + "</strong> moves</p>";
 
     modal.style.display = "inline";
 }
 
-//tracks & displays number of moves player makes 
+//Tracks & displays number of moves player makes 
 function movesCount(){
     playerMovesCount += 1;
     playerMoves.innerHTML = playerMovesCount;
 }
-//resets player moves
+//Resets player moves
 function resetMovesCount(){
     playerMovesCount = 0;
     playerMoves.innerHTML = playerMovesCount;
 }
 
-//array of matched cards/pairs
+//Array of matched cards/pairs
 function myCardMatches(){
     myMatches.push(cardsOpened[0]);
     myMatches.push(cardsOpened[1]);
 }
-//resets array of matched cards
+//Resets array of matched cards
 function resetMyCardMatches(){
     myMatches.splice(0);
 }
 
-//Game Timer Function
+//Closes unmatched cards & clears the cardsOpened array
+function notAMatch(){
+    cardsOpened[0].classList = "";
+    cardsOpened[1].classList = "";
+    cardsOpened[0].classList += "card";
+    cardsOpened[1].classList += "card";
+    clearArray();
+}
+
+//Opens selected cards
+function openCards(){
+    cardsOpened[0].classList = "";
+    cardsOpened[1].classList = "";
+    cardsOpened[0].classList += "card open show";
+    cardsOpened[1].classList += "card open show";
+}
+
+//Deducts points for incorrect guesses & for use with starRating function
+function playerPenaltyFunc(){
+   playerPenaltyPoints += 2;
+}
+//Resets penalty points 
+function resetPenaltyPoints(){
+    playerPenaltyPoints = 0;
+}
+
+//Tracks player points for correct guesses
+function pointsEarned   (){
+    playerPoints += 12.5;
+    totalPoints = playerPoints - playerPenaltyPoints;
+}
+//Resets playerPoints 
+function resetPointsEarned(){
+    playerPoints = 0;
+    totalPoints = 0;
+}
+
+//Resets game board
+function resetGameBoard(){
+    for(let i = 0; i < indexedCards.length; i++){
+        indexedCards[i].className = "";
+        indexedCards[i].className = "card";
+    }
+}
+
+//Star rating function
+const starRating = function(){
+    if(incorrectGuesses === 0 || incorrectGuesses <= 4){
+        scoreStars[0].style.display= "inline";
+        scoreStars[1].style.display = "inline";
+        scoreStars[2].style.display = "inline";
+        finalStarRating = 3;
+    } else if(incorrectGuesses >= 5 && incorrectGuesses <= 10){
+        scoreStars[0].style.display = "inline";
+        scoreStars[1].style.display = "inline";
+        scoreStars[2].style.display = "none";
+        finalStarRating = 2;
+    } else if(incorrectGuesses > 10){
+        scoreStars[0].style.display = "inline";
+        scoreStars[1].style.display = "none";
+        scoreStars[2].style.color = "none";
+        finalStarRating = 1;
+    } 
+}
+//restart star rating 
+function resetStarRating(){
+    for(let i = 0; i < scoreStars.length; i++){
+        scoreStars[i].style.display = "inline";
+    }
+}
+
+//Game Timer Function(s)
 //Adopted from Daniel Hug's JS Stopwatch: https://jsfiddle.net/Daniel_Hug/pvk6p/
 function startTimer(){
     time = setTimeout(gameTimer, 1000);
 }
+
 function gameTimer(){
     seconds++;
     if(seconds >= 60){
@@ -322,86 +397,3 @@ function resetMyTimer(){
     hours = 0;
     gameTimerDisplay.innerHTML = hours + "0:0" + minutes + ":0" + seconds;
 }
-
-//closes unmatched cards & clears the cardsOpened array
-function notAMatch(){
-    cardsOpened[0].classList = "";
-    cardsOpened[1].classList = "";
-    cardsOpened[0].classList += "card";
-    cardsOpened[1].classList += "card";
-    clearArray();
-}
-
-//opens selected cards
-function openCards(){
-    cardsOpened[0].classList = "";
-    cardsOpened[1].classList = "";
-    cardsOpened[0].classList += "card open show";
-    cardsOpened[1].classList += "card open show";
-}
-
-//deducts points for incorrect guesses & for use with starRating function
-function playerPenaltyFunc(){
-   playerPenaltyPoints += 2;
-}
-//resets penalty points 
-function resetPenaltyPoints(){
-    playerPenaltyPoints = 0;
-}
-
-//tracks player points for correct guesses
-function pointsEarned   (){
-    playerPoints += 12.5;
-    totalPoints = playerPoints - playerPenaltyPoints;
-}
-//resets playerPoints 
-function resetPointsEarned(){
-    playerPoints = 0;
-    totalPoints = 0;
-}
-
-//resets game board
-function resetGameBoard(){
-    for(let i = 0; i < indexedCards.length; i++){
-        indexedCards[i].className = "";
-        indexedCards[i].className = "card";
-    }
-}
-
-//star rating
-const starRating = function(){
-    if(incorrectGuesses === 0 || incorrectGuesses <= 4){
-        scoreStars[0].style.display= "inline";
-        scoreStars[1].style.display = "inline";
-        scoreStars[2].style.display = "inline";
-        finalStarRating = 3;
-    } else if(incorrectGuesses >= 5 && incorrectGuesses <= 10){
-        scoreStars[0].style.display = "inline";
-        scoreStars[1].style.display = "inline";
-        scoreStars[2].style.display = "none";
-        finalStarRating = 2;
-    } else if(incorrectGuesses > 10){
-        scoreStars[0].style.display = "inline";
-        scoreStars[1].style.display = "none";
-        scoreStars[2].style.color = "none";
-        finalStarRating = 1;
-    } 
-}
-//restart star rating 
-function resetStarRating(){
-    for(let i = 0; i < scoreStars.length; i++){
-        scoreStars[i].style.display = "inline";
-    }
-}
-//My logic endds here
-
-/* UDACITY'S NOTES
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
